3e76a1a2e9158cd3cdb1c9dd75889027d15fdb4d
jagm <jagiela.mateusz@gmail.com>
2013-01-12 19:05:21 +0100

    * add hibernate to project * add simple model for configuration

---
+++ src/main/java/pl/jagm/kanban/model/Configuration.java
@@ -0,0 +1,74 @@
+package pl.jagm.kanban.model;
+
+import org.jetbrains.annotations.NotNull;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Configuration {
+
+    private Long id;
+    private String name = "";
+    private String value = "";
+
+    public Configuration() {
+    }
+
+    public Configuration(@NotNull String name, @NotNull String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @NotNull
+    @Column(nullable = false)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(@NotNull String name) {
+        this.name = name;
+    }
+
+    @NotNull
+    @Column(nullable = false)
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(@NotNull String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Configuration that = (Configuration) o;
+
+        if (!name.equals(that.name)) return false;
+        if (!value.equals(that.value)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + value.hashCode();
+        return result;
+    }
+}
