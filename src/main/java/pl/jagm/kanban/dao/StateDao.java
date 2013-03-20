803bd1339faeb2e70f0ee6f8eac6630e87c4a583
jagm <jagiela.mateusz@gmail.com>
2013-03-20 21:40:41 +0100

    add dao for State

---
+++ src/main/java/pl/jagm/kanban/dao/StateDao.java
@@ -0,0 +1,13 @@
+package pl.jagm.kanban.dao;
+
+import org.jetbrains.annotations.NotNull;
+import pl.jagm.kanban.model.IssueState;
+import pl.jagm.kanban.model.State;
+
+public interface StateDao {
+
+    public State read(@NotNull final int id);
+
+    public void createIsueState(@NotNull final IssueState issueState);
+
+}
---
+++ src/main/java/pl/jagm/kanban/dao/StateDaoImpl.java
@@ -0,0 +1,22 @@
+package pl.jagm.kanban.dao;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import pl.jagm.kanban.model.IssueState;
+import pl.jagm.kanban.model.State;
+
+@Component
+@Transactional
+public class StateDaoImpl extends AbstractDao implements StateDao {
+
+    @Override
+    public State read(@NotNull int id) {
+        return (State) getCurrentSession().get(State.class, id);
+    }
+
+    @Override
+    public void createIsueState(@NotNull IssueState issueState) {
+        getCurrentSession().persist(issueState);
+    }
+}
