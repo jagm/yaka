bd1cae46126ee1d79879ae804cdd7cd076a30bd6
jagm <jagiela.mateusz@gmail.com>
2013-10-06 15:13:38 +0200

    extract post request to method and add error handling in js

--- src/main/resources/js/controllers.js
+++ src/main/resources/js/controllers.js
@@ -1,13 +1,30 @@
 KanbanApp.Controllers = (function () {
 
-    var FORM_HTTP_HEADER = { headers: {'Content-Type': 'application/x-www-form-urlencoded'} };
+    var errorHandling = function (data) {
+        KanbanApp.Notify.error(data.errorMessage, "Error occurred");
+    };
+
+    var postRequest = function ($http, url, params, successCallback) {
+
+        var FORM_HTTP_HEADER = { headers: {'Content-Type': 'application/x-www-form-urlencoded'} };
+
+        $http.post(
+                KanbanApp.Configuration.contextPath + url,
+                $.param(params),
+                FORM_HTTP_HEADER
+            )
+            .error(errorHandling)
+            .success(successCallback);
+    }
 
     return {
 
         BoardListCtrl: function ($scope, $rootScope, $http) {
-            $http.get(KanbanApp.Configuration.contextPath + '/board/list').success(function (data) {
-                $rootScope.boards = data;
-            });
+            $http.get(KanbanApp.Configuration.contextPath + '/board/list')
+                .error(errorHandling)
+                .success(function (data) {
+                    $rootScope.boards = data;
+                });
         },
 
         BoardDetailCtrl: function ($http, $scope, $routeParams, $rootScope, board, issues) {
@@ -20,51 +37,38 @@ KanbanApp.Controllers = (function () {
             $scope.issues = issues;
 
             $scope.changeState = function (issueId, stateId) {
-                $http.post(
-                        KanbanApp.Configuration.contextPath + '/issues/change-state',
-                        $.param({issue_id: issueId, state_id: stateId}),
-                        FORM_HTTP_HEADER
-                    ).success(function (data) {
-                        if (data.errors && data.errors.length) {
-                            KanbanApp.Notify.error(data.errors, 'error!!1')
-                        } else {
-
-                        }
-                        console.log(data);
-                    });
+                postRequest($http, '/issues/change-state', {issue_id: issueId, state_id: stateId}, function (data) {
+                    if (data.errors && data.errors.length) {
+                        KanbanApp.Notify.error(data.errors, 'error!!1')
+                    } else {
+                    }
+                    console.log(data);
+                });
             };
 
             $scope.changeOrder = function (issues) {
                 var order = issues.map(function (issue) {
                     return parseInt(issue.replace('issue_', ''), 10);
                 });
-                $http.post(
-                        KanbanApp.Configuration.contextPath + '/issues/change-order',
-                        $.param({order: order}),
-                        FORM_HTTP_HEADER
-                    ).success(function (data) {
-                        if (data.errors && data.errors.length) {
-                            KanbanApp.Notify.error(data.errors, 'error!!1')
-                        } else {
+                postRequest($http, '/issues/change-order', {order: order}, function (data) {
+                    if (data.errors && data.errors.length) {
+                        KanbanApp.Notify.error(data.errors, 'error!!1')
+                    } else {
 
-                        }
-                        console.log(data);
-                    });
+                    }
+                    console.log(data);
+                });
             };
 
             $scope.delete = function (issueId) {
-                $http.post(
-                        KanbanApp.Configuration.contextPath + '/issues/delete',
-                        $.param({issue_id: issueId}),
-                        FORM_HTTP_HEADER
-                    ).success(function (data) {
-                        if (data) {
-                            $scope.issues = $scope.issues.filter(function (issue) {
-                                return issue.id !== issueId;
-                            })
-                        }
-                        console.log('[Issue delete][response]: ', data);
-                    });
+                postRequest($http, '/issues/delete', {issue_id: issueId}, function (data) {
+                    if (data) {
+                        $scope.issues = $scope.issues.filter(function (issue) {
+                            return issue.id !== issueId;
+                        })
+                    }
+                    console.log('[Issue delete][response]: ', data);
+                });
             }
 
         },
@@ -72,17 +76,21 @@ KanbanApp.Controllers = (function () {
         BoardDetailCtrlResolve: {
             board: function ($http, $q, $route) {
                 var deferred = $q.defer();
-                $http.get(KanbanApp.Configuration.contextPath + '/board/read/' + $route.current.params.boardId).success(function (data) {
-                    deferred.resolve(data);
-                });
+                $http.get(KanbanApp.Configuration.contextPath + '/board/read/' + $route.current.params.boardId)
+                    .error(errorHandling)
+                    .success(function (data) {
+                        deferred.resolve(data);
+                    });
                 return deferred.promise;
 
             },
             issues: function ($http, $q, $route) {
                 var deferred = $q.defer();
-                $http.get(KanbanApp.Configuration.contextPath + '/issues/list/' + $route.current.params.boardId).success(function (data) {
-                    deferred.resolve(data);
-                });
+                $http.get(KanbanApp.Configuration.contextPath + '/issues/list/' + $route.current.params.boardId)
+                    .error(errorHandling)
+                    .success(function (data) {
+                        deferred.resolve(data);
+                    });
                 return deferred.promise;
             }
         },
@@ -95,18 +103,14 @@ KanbanApp.Controllers = (function () {
             $scope.save = function (issue) {
                 issue.state_id = $scope.board.states[0].id;
                 console.log(issue);
-                $http.post(
-                        KanbanApp.Configuration.contextPath + '/issues/add',
-                        $.param(issue),
-                        FORM_HTTP_HEADER
-                    ).success(function (data) {
-                        if (data.errors && data.errors.length) {
-                            KanbanApp.Notify.error(data.errors, 'error!!1')
-                        } else {
+                postRequest($http, '/issues/add', issue, function (data) {
+                    if (data.errors && data.errors.length) {
+                        KanbanApp.Notify.error(data.errors, 'error!!1')
+                    } else {
 
-                        }
-                        console.log(data);
-                    });
+                    }
+                    console.log(data);
+                });
             };
         },
 
@@ -116,41 +120,33 @@ KanbanApp.Controllers = (function () {
             };
 
             $scope.save = function (state) {
-                $http.post(
-                        KanbanApp.Configuration.contextPath + '/board/add-state',
-                        $.param(state),
-                        FORM_HTTP_HEADER
-                    ).success(function (data) {
-                        if (data.errors && data.errors.length) {
+                postRequest($http, '/board/add-state', state, function (data) {
+                    if (data.errors && data.errors.length) {
 
-                            KanbanApp.Notify.error(data.errors, 'error!!1')
-                        } else {
-                            console.log('success!');
-                            $route.reload();
-                        }
-                        console.log(data);
-                    });
+                        KanbanApp.Notify.error(data.errors, 'error!!1')
+                    } else {
+                        console.log('success!');
+                        $route.reload();
+                    }
+                    console.log(data);
+                });
             };
         },
 
         AddBoardCtrl: function ($scope, $http, $rootScope) {
             $scope.board = {};
             $scope.save = function (board) {
-                $http.post(
-                        KanbanApp.Configuration.contextPath + '/board/add',
-                        $.param(board),
-                        FORM_HTTP_HEADER
-                    ).success(function (data) {
-                        if (data.errors && data.errors.length) {
-                            KanbanApp.Notify.error(data.errors.map(function (el) {
-                                return el.field + ' ' + el.defaultMessage;
-                            }), 'error!!!');
-                        } else {
-                            $('#add-board-modal').modal('hide');
-                            $rootScope.boards = data.boards;
-                        }
-                        console.log(data);
-                    });
+                postRequest($http, '/board/add', board, function (data) {
+                    if (data.errors && data.errors.length) {
+                        KanbanApp.Notify.error(data.errors.map(function (el) {
+                            return el.field + ' ' + el.defaultMessage;
+                        }), 'error!!!');
+                    } else {
+                        $('#add-board-modal').modal('hide');
+                        $rootScope.boards = data.boards;
+                    }
+                    console.log(data);
+                });
             };
         }
 
